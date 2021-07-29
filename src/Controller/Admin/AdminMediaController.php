343b293987358df578982d2bc12519420fcb9cc9
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-29 14:50:23 +0200

    Delete Media

--- src/Controller/Admin/AdminMediaController.php
+++ src/Controller/Admin/AdminMediaController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 
 
 use App\Repository\MediaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +20,21 @@ class AdminMediaController extends AbstractController
         $medias = $mediaRepository->findAll();
         $medias6 = $paginator->paginate($medias, $request->query->getInt('page', 1), 6);
         return $this->render('admin/medias.html.twig',['medias' => $medias6]);
+    }
+
+    /**
+     * @Route("/admin/media/delete/{id}", name="admin_delete_media")
+     */
+    public function deleteMedia($id, MediaRepository $mediaRepository, EntityManagerInterface $entityManager)
+    {
+        $media = $mediaRepository->find($id);
+        $entityManager->remove($media);
+        $entityManager->flush();
+        $this->addFlash(
+            'notice',
+            'Votre image est supprimé');
+
+        return $this->redirectToRoute('admin_list_medias');
     }
 
 }
