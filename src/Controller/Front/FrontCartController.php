d887cc966590b214c507df0913156573aedbbcb0
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-08-03 11:26:30 +0200

    Début panier controller et session

---
+++ src/Controller/Front/FrontCartController.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Controller\Front;
+
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class FrontCartController extends AbstractController
+{
+
+    /**
+     * @Route("/cart/", name="cart")
+     */
+    public function indexCart(SessionInterface $session, ProductRepository $productRepository)
+    {
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+        foreach ($panier as $id => $quantity){
+            $panierWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        dd($panierWithData);
+        return $this->render('front/panier.html.twig', ['items' => $panierWithData]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="add_cart")
+     */
+    public function addCart($id, SessionInterface $session)
+    {
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id] = 1;
+        }
+
+
+        $session->set('panier', $panier);
+
+        dd($session->get('panier'));
+    }
+
+
+}
