107d2b5f8e703d327eaf03ee43bc11d7342ea765
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-08-20 13:44:23 +0200

    Comment contenu

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
@@ -19,6 +20,13 @@ class Comment
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Veuillez remplir le contenue du commentaire")
+     * @Assert\Length(
+     *     min = 10,
+     *     max = 200,
+     *     minMessage="Votre commentaire doit faire au moins 10 caractères.",
+     *     maxMessage="Votre commentaire doit faire au plus 200 caractères"
+     * )
      */
     private $content;
 
